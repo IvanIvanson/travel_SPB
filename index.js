8d07ce168fcccac4103859fe473c41bf74c58f39
ivan <ghalut@mail.ru>
2022-05-05 20:00:13 +0300

    project1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let btnVideo = document.querySelector("#btnPlay");
+let btnVideo = document.querySelector("#btnVideo");
+let btnPlay = document.querySelector("#btnPlay");
+let caption = document.querySelector(".caption");
+
 let img = document.querySelector("#imgVideo");
 
 $("#btnPlay").click(function () {
   let video = '<iframe src="' + $(img).attr("data-video") + '"></iframe>';
-
+  btnVideo.style.display = "none";
+  btnPlay.style.display = "none";
+  caption.style.display = "none";
   $(img).replaceWith(video);
 });
 
